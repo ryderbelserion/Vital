9526558e03a85acee78331a8339a7ffb1b697be6
Ryder Belserion <no-reply@ryderbelserion.com>
2024-05-24 16:13:01 -0400

    add publish task back

--- buildSrc/src/main/kotlin/java-plugin.gradle.kts
+++ buildSrc/src/main/kotlin/java-plugin.gradle.kts
@@ -27,3 +27,81 @@ feather {
         javaVersion(21)
     }
 }
+
+val javaComponent: SoftwareComponent = components["java"]
+
+tasks {
+    val sourcesJar by creating(Jar::class) {
+        archiveClassifier.set("sources")
+        from(sourceSets.main.get().allSource)
+    }
+
+    val javadocJar by creating(Jar::class) {
+        dependsOn.add(javadoc)
+        archiveClassifier.set("javadoc")
+        from(javadoc)
+    }
+
+    publishing {
+        repositories {
+            maven {
+                url = uri("https://repo.crazycrew.us/releases")
+
+                credentials {
+                    this.username = System.getenv("gradle_username")
+                    this.password = System.getenv("gradle_password")
+                }
+            }
+        }
+
+        publications {
+            create<MavenPublication>("maven") {
+                from(javaComponent)
+
+                artifact(sourcesJar)
+                artifact(javadocJar)
+
+                group = "com.ryderbelserion.vital"
+                artifactId = project.name.lowercase()
+                version = "${rootProject.version}"
+
+                versionMapping {
+                    usage("java-api") {
+                        fromResolutionOf("runtimeClasspath")
+                    }
+
+                    usage("java-runtime") {
+                        fromResolutionResult()
+                    }
+                }
+
+                pom {
+                    name.set("Vital API")
+                    description.set("A library with a slew of things")
+                    url.set("https://github.com/ryderbelserion/Vital")
+
+                    licenses {
+                        licenses {
+                            name.set("MIT")
+                            url.set("https://opensource.org/licenses/MIT")
+                        }
+                    }
+
+                    developers {
+                        developer {
+                            id.set("ryderbelserion")
+                            name.set("Ryder Belserion")
+                            email.set("no-reply@ryderbelserion.com")
+                        }
+                    }
+
+                    scm {
+                        connection.set("scm:git:git://github.com/ryderbelserion/Vital")
+                        developerConnection.set("scm:git:ssh://github.com/ryderbelserion/Vital")
+                        url.set("https://github.com/ryderbelserion/Vital")
+                    }
+                }
+            }
+        }
+    }
+}
