d531abec0eb3e1af7095970d305d41d3c103529e
Ryder Belserion <no-reply@ryderbelserion.com>
2024-06-08 11:33:29 -0400

    Clean up example plugin

--- example/src/main/java/com/ryderbelserion/vital/TestPlugin.java
+++ example/src/main/java/com/ryderbelserion/vital/TestPlugin.java
@@ -1,35 +1,12 @@
 package com.ryderbelserion.vital;
 
-import com.ryderbelserion.vital.core.config.YamlManager;
 import com.ryderbelserion.vital.paper.VitalPaper;
-import com.ryderbelserion.vital.paper.builders.items.ItemBuilder;
-import org.bukkit.Material;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TestPlugin extends JavaPlugin implements Listener {
+public class TestPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getDataFolder().mkdirs();
-
         new VitalPaper(this);
-
-        YamlManager manager = new YamlManager();
-
-        manager.addFolder("crates").init();
-
-        getServer().getPluginManager().registerEvents(this, this);
-    }
-
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        ItemBuilder itemBuilder = new ItemBuilder().withType(Material.PLAYER_HEAD);
-
-        itemBuilder.setPlayer("Rukkhadevata");
-
-        event.getPlayer().getInventory().addItem(itemBuilder.getStack());
     }
 }
