4701539b29820e6e2a3fd6f7994baa602cc71fc5
Ryder Belserion <no-reply@ryderbelserion.com>
2024-06-25 19:24:36 -0400

    add maven publish

--- buildSrc/src/main/kotlin/kotlin-plugin.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-plugin.gradle.kts
@@ -1,8 +1,11 @@
 import com.ryderbelserion.feather.enums.Repository
 import gradle.kotlin.dsl.accessors._5eb79900c8ae59d7bb3a62f4246cfb92.feather
+import gradle.kotlin.dsl.accessors._5eb79900c8ae59d7bb3a62f4246cfb92.publishing
 
 plugins {
     id("com.ryderbelserion.feather-core")
+
+    `maven-publish`
 
     kotlin("jvm")
 }
@@ -26,5 +29,69 @@ feather {
         javaSource(JvmVendorSpec.ADOPTIUM)
 
         javaVersion(21)
+    }
+}
+
+val javaComponent: SoftwareComponent = components["java"]
+
+tasks {
+    publishing {
+        repositories {
+            maven {
+                url = uri("https://repo.crazycrew.us/releases")
+
+                credentials {
+                    this.username = System.getenv("gradle_username")
+                    this.password = System.getenv("gradle_password")
+                }
+            }
+        }
+
+        publications {
+            create<MavenPublication>("maven") {
+                from(javaComponent)
+
+                group = "com.ryderbelserion.vital"
+                artifactId = project.name.lowercase()
+                version = "${rootProject.version}"
+
+                versionMapping {
+                    usage("java-api") {
+                        fromResolutionOf("runtimeClasspath")
+                    }
+
+                    usage("java-runtime") {
+                        fromResolutionResult()
+                    }
+                }
+
+                pom {
+                    name.set("Vital API")
+                    description.set("A library with a slew of things")
+                    url.set("https://github.com/ryderbelserion/Vital")
+
+                    licenses {
+                        licenses {
+                            name.set("MIT")
+                            url.set("https://opensource.org/licenses/MIT")
+                        }
+                    }
+
+                    developers {
+                        developer {
+                            id.set("ryderbelserion")
+                            name.set("Ryder Belserion")
+                            email.set("no-reply@ryderbelserion.com")
+                        }
+                    }
+
+                    scm {
+                        connection.set("scm:git:git://github.com/ryderbelserion/Vital")
+                        developerConnection.set("scm:git:ssh://github.com/ryderbelserion/Vital")
+                        url.set("https://github.com/ryderbelserion/Vital")
+                    }
+                }
+            }
+        }
     }
 }
