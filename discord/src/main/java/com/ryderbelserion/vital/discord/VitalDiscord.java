9ca9d07466edfc5b4b2513b56ff6e11a79c4517b
Ryder Belserion <no-reply@ryderbelserion.com>
2024-06-26 03:02:16 -0400

    extend vital class

--- discord/src/main/java/com/ryderbelserion/vital/discord/VitalDiscord.java
+++ discord/src/main/java/com/ryderbelserion/vital/discord/VitalDiscord.java
@@ -1,20 +1,26 @@
 package com.ryderbelserion.vital.discord;
 
+import com.ryderbelserion.vital.core.Vital;
 import com.ryderbelserion.vital.discord.listeners.GenericListener;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
+import org.jetbrains.annotations.NotNull;
 import java.io.File;
 import java.util.List;
+import java.util.logging.Logger;
 
-public abstract class VitalDiscord {
+public abstract class VitalDiscord extends Vital {
 
+    private final Logger logger;
     private final File file;
     private final JDA jda;
 
-    public VitalDiscord(final String folder, final String token, final List<GatewayIntent> intents, final List<CacheFlag> flags) {
+    public VitalDiscord(final Logger logger, final String folder, final String token, final List<GatewayIntent> intents, final List<CacheFlag> flags) {
+        this.logger = logger;
+
         this.file = new File(folder);
 
         this.jda = JDABuilder.createDefault(token, intents).enableCache(flags).addEventListeners(new GenericListener(this)).build();
@@ -28,8 +34,19 @@ public abstract class VitalDiscord {
 
     public abstract void stop();
 
-    protected final File getDirectory() {
+    @Override
+    public @NotNull final File getDirectory() {
         return this.file;
+    }
+
+    @Override
+    public boolean isLogging() {
+        return true;
+    }
+
+    @Override
+    public @NotNull Logger getLogger() {
+        return this.logger;
     }
 
     protected final JDA getJDA() {
