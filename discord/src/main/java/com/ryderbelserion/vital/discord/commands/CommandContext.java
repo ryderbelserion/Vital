af2dc8461b3b98592c2bdb5c6ace7a60e65a6d27
Ryder Belserion <no-reply@ryderbelserion.com>
2024-06-26 10:50:36 -0400

    add simple prefixed commands

--- discord/src/main/java/com/ryderbelserion/vital/discord/commands/CommandContext.java
+++ discord/src/main/java/com/ryderbelserion/vital/discord/commands/CommandContext.java
@@ -7,60 +7,93 @@ import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.SelfUser;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import org.jetbrains.annotations.Nullable;
 
 public class CommandContext implements CommandActor {
 
-    private final SlashCommandInteractionEvent event;
+    private SlashCommandInteractionEvent slash;
+    private MessageReceivedEvent message;
 
-    public CommandContext(final SlashCommandInteractionEvent event) {
-        this.event = event;
+    public CommandContext(@Nullable final SlashCommandInteractionEvent slash, @Nullable final MessageReceivedEvent message) {
+        if (message != null) {
+            this.message = message;
+        }
+
+        if (slash != null) {
+            this.slash = slash;
+        }
+    }
+
+    public final boolean isSlashActive() {
+        return this.slash != null;
+    }
+
+    public final boolean isMessageActive() {
+        return this.message == null;
     }
 
     @Override
     public void reply(final String message, final boolean ephemeral) {
-        this.event.reply(message).setEphemeral(ephemeral).queue();
+        if (isSlashActive()) {
+            this.slash.reply(message).setEphemeral(ephemeral).queue();
+
+            return;
+        }
+
+        this.message.getChannel().sendMessage(message).queue();
     }
 
     @Override
     public void reply(final MessageEmbed message, final boolean ephemeral) {
-        this.event.replyEmbeds(message).setEphemeral(ephemeral).queue();
+        if (isSlashActive()) {
+            this.slash.replyEmbeds(message).setEphemeral(ephemeral).queue();
+
+            return;
+        }
+
+        this.message.getChannel().sendMessageEmbeds(message).queue();
     }
 
     @Override
-    public CommandContext defer(final boolean ephemeral) {
-        this.event.deferReply(ephemeral).queue();
+    public final CommandContext defer(final boolean ephemeral) {
+        if (isSlashActive()) {
+            this.slash.deferReply(ephemeral).queue();
+
+            return this;
+        }
 
         return this;
     }
 
     @Override
-    public OptionMapping getOption(final String option) {
-        return this.event.getOption(option);
+    public @Nullable final OptionMapping getOption(final String option) {
+        return isSlashActive() ? this.slash.getOption(option) : null;
     }
 
     @Override
-    public User getAuthor() {
-        return this.event.getUser();
+    public final User getAuthor() {
+        return isSlashActive() ? this.slash.getUser() : this.message.getAuthor();
     }
 
     @Override
-    public User getCreator(final long id) {
+    public final User getCreator(final long id) {
         return getJDA().getUserById(id);
     }
 
     @Override
-    public SelfUser getBot() {
+    public final SelfUser getBot() {
         return getJDA().getSelfUser();
     }
 
     @Override
-    public Guild getGuild() {
-        return this.event.getGuild();
+    public final Guild getGuild() {
+        return isSlashActive() ? this.slash.getGuild() : this.message.getGuild();
     }
 
     @Override
-    public JDA getJDA() {
-        return this.event.getJDA();
+    public final JDA getJDA() {
+        return isSlashActive() ? this.slash.getJDA() : this.message.getJDA();
     }
 }
--- discord/src/main/java/com/ryderbelserion/vital/discord/commands/CommandEngine.java
+++ discord/src/main/java/com/ryderbelserion/vital/discord/commands/CommandEngine.java
@@ -1,7 +1,9 @@
 package com.ryderbelserion.vital.discord.commands;
 
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
@@ -21,9 +23,20 @@ public abstract class CommandEngine extends ListenerAdapter {
 
     @Override
     public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
-        CommandContext context = new CommandContext(event);
+        CommandContext context = new CommandContext(event, null);
 
         if (!event.getName().equals(this.name)) return;
+
+        perform(context);
+    }
+
+    @Override
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+        final CommandContext context = new CommandContext(null, event);
+
+        final Message message = event.getMessage();
+
+        if (!message.getContentRaw().startsWith(this.name)) return;
 
         perform(context);
     }
