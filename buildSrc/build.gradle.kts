b774e1826536963d74835e2d9b62cc108f43dd39
Ryder Belserion <no-reply@ryderbelserion.com>
2024-06-28 00:02:46 -0400

    update buildscript

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,7 +1,13 @@
+import com.ryderbelserion.feather.feather
+
 plugins {
+    id("com.ryderbelserion.feather-logic") version "0.0.1"
+
     `kotlin-dsl`
 }
 
 dependencies {
     implementation(libs.kotlin)
+
+    feather("0.0.1")
 }
--- discord/build.gradle.kts
+++ discord/build.gradle.kts
@@ -4,8 +4,6 @@ plugins {
     `java-plugin`
 }
 
-project.version = "1.8.2"
-
 dependencies {
     api(projects.core)
 
@@ -18,24 +16,8 @@ dependencies {
     api(libs.jda)
 }
 
-val javaComponent: SoftwareComponent = components["java"]
-
 tasks {
-    publishing {
-        publications {
-            create<MavenPublication>("maven") {
-                from(javaComponent)
-
-                group = "com.ryderbelserion.vital"
-                artifactId = project.name.lowercase()
-                version = "${project.version}"
-            }
-        }
-    }
-
     shadowJar {
-        mergeServiceFiles()
-
         manifest {
             from(project(":core").tasks.named<Jar>("shadowJar").get().manifest)
         }
