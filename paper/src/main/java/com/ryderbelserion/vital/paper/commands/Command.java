8a0112c62be3c9294257ea334693d690e03860bb
Ryder Belserion <no-reply@ryderbelserion.com>
2024-07-30 20:34:23 -0400

    add command module

---
+++ paper/src/main/java/com/ryderbelserion/vital/paper/commands/Command.java
@@ -0,0 +1,91 @@
+package com.ryderbelserion.vital.paper.commands;
+
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import com.mojang.brigadier.tree.LiteralCommandNode;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import org.jetbrains.annotations.NotNull;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * A command object class to provide useful utilities and enforcement on methods.
+ *
+ * @author Ryder Belserion
+ * @version 2.4
+ * @since 2.4
+ */
+public abstract class Command {
+
+    /**
+     * Executes the command while passing through {@link CommandData}
+     *
+     * @param data {@link CommandData}
+     */
+    public abstract void execute(CommandData data);
+
+    /**
+     * Get the permission required to execute the command.
+     *
+     * @return the permission node
+     */
+    public abstract @NotNull String getPermission();
+
+    /**
+     * Get the command literal which is effectively what builds the command.
+     *
+     * @return {@link LiteralCommandNode}
+     */
+    public abstract @NotNull LiteralCommandNode<CommandSourceStack> literal();
+
+    /**
+     * Registers the permission to the server.
+     *
+     * @return {@link Command}
+     */
+    public abstract @NotNull Command registerPermission();
+
+    /**
+     * Return a list of uuids for the suggestion provider.
+     *
+     * @param builder {@link SuggestionsBuilder}
+     * @return a list of suggestions
+     */
+    public @NotNull final CompletableFuture<Suggestions> suggestNames(final SuggestionsBuilder builder) {
+        for (int count = 1; count <= 7; count++) builder.suggest(UUID.randomUUID().toString().replace("-", "").substring(0, 8));
+
+        return builder.buildFuture();
+    }
+
+    /**
+     * Return a list of Integers for the suggestion provider.
+     *
+     * @param builder {@link SuggestionsBuilder}
+     * @return a list of suggestions
+     */
+    public @NotNull final CompletableFuture<Suggestions> suggestIntegers(final SuggestionsBuilder builder) {
+        for (int count = 1; count <= 100; count++) builder.suggest(count);
+
+        return builder.buildFuture();
+    }
+
+    /**
+     * Return a list of Doubles for the suggestion provider.
+     *
+     * @param builder {@link SuggestionsBuilder}
+     * @return a list of suggestions
+     */
+    public @NotNull final CompletableFuture<Suggestions> suggestDoubles(final SuggestionsBuilder builder) {
+        int count = 0;
+
+        while (count <= 1000) {
+            double x = count / 10.0;
+
+            builder.suggest(String.valueOf(x));
+
+            count++;
+        }
+
+        return builder.buildFuture();
+    }
+}
---
+++ paper/src/main/java/com/ryderbelserion/vital/paper/commands/CommandData.java
@@ -0,0 +1,104 @@
+package com.ryderbelserion.vital.paper.commands;
+
+import com.mojang.brigadier.context.CommandContext;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * A command data class to provide some abstraction to brigadier commands
+ *
+ * @author Ryder Belserion
+ * @version 2.4
+ * @since 2.4
+ */
+public class CommandData {
+
+    private final CommandContext<CommandSourceStack> context;
+
+    /**
+     * A constructor to apply the command context
+     *
+     * @param context {@link CommandContext}
+     */
+    public CommandData(@NotNull final CommandContext<CommandSourceStack> context) {
+        this.context = context;
+    }
+
+    /**
+     * Gets the command source stack from {@link CommandContext}
+     *
+     * @return {@link CommandSourceStack}
+     */
+    public @NotNull final CommandSourceStack getSource() {
+        return this.context.getSource();
+    }
+
+    /**
+     * Gets the argument value from the name of the argument
+     *
+     * @param key the name of the argument
+     * @return the argument value
+     */
+    public @NotNull final String getStringArgument(@NotNull final String key) {
+        return this.context.getArgument(key, String.class);
+    }
+
+    /**
+     * Gets the argument value from the name of the argument
+     *
+     * @param key the name of the argument
+     * @return the argument value
+     */
+    public final int getIntegerArgument(@NotNull final String key) {
+        return this.context.getArgument(key, Integer.class);
+    }
+
+    /**
+     * Gets the argument value from the name of the argument
+     *
+     * @param key the name of the argument
+     * @return the argument value
+     */
+    public final float getFloatArgument(@NotNull final String key) {
+        return this.context.getArgument(key, Float.class);
+    }
+
+    /**
+     * Gets the argument value from the name of the argument
+     *
+     * @param key the name of the argument
+     * @return the argument value
+     */
+    public final double getDoubleArgument(@NotNull final String key) {
+        return this.context.getArgument(key, Double.class);
+    }
+
+    /**
+     * Gets the {@link CommandSender} from the {@link CommandSourceStack}.
+     *
+     * @return {@link CommandSender}
+     */
+    public @NotNull final CommandSender getCommandSender() {
+        return getSource().getSender();
+    }
+
+    /**
+     * Gets the {@link Player} from the {@link CommandSourceStack}.
+     *
+     * @return {@link Player}
+     */
+    public @NotNull final Player getPlayer() {
+        return (Player) getCommandSender();
+    }
+
+    /**
+     * Checks if the {@link CommandSender} is a {@link Player}
+     *
+     * @return true or false
+     */
+    public final boolean isPlayer() {
+        return getCommandSender() instanceof Player;
+    }
+}
