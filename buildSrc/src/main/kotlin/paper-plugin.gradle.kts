0bfab069573ec53423069a7dfcae0613ce119cf4
Ryder Belserion <no-reply@ryderbelserion.com>
2024-09-18 01:38:31 -0400

    depend on the official papi repository again

--- buildSrc/src/main/kotlin/paper-plugin.gradle.kts
+++ buildSrc/src/main/kotlin/paper-plugin.gradle.kts
@@ -5,5 +5,7 @@ plugins {
 repositories {
     maven("https://repo.papermc.io/repository/maven-public")
 
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi")
+
     maven("https://repo.oraxen.com/releases")
 }
